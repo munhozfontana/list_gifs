0fcd9b34f05a7e5060008ea23be9f48391f38867
luís fernando <munhozfontana@gmail.com>
2019-11-28 23:41:05 -0300

    requisição de dados

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +9,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((res) {
+      print(res);
+    });
+  }
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+    if (_search == null) {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=mc6wcQs7bRC9xp5VfCl6nxna7unqMS0S&limit=25&rating=G");
+    } else {
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=mc6wcQs7bRC9xp5VfCl6nxna7unqMS0S&q=$_search&limit=25&offset=$_offset&rating=G&lang=pt");
+    }
+    return json.decode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
